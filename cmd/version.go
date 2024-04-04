a16cf0ff53b5fd45136fb9c762db496a9e94c093
mintniron <164421962+mintniron@users.noreply.github.com>
2024-04-04 21:22:08 +0000

    p2

--- cmd/version.go
+++ cmd/version.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -9,6 +8,9 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+// appVersion App Version
+var appVersion="Version"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -20,8 +22,8 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+Run: func(cmd *cobra.Command, args []string) {
+	fmt.Println(appVersion)
 	},
 }
 
