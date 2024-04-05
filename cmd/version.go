8acffcd41d991516862e001b35e6e3b4e90dbe20
Vlodko Sikach <qttdev004@gmail.com>
2024-04-05 03:05:52 +0300

    var add

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ import (
 )
 
 // appVersion App Version
-var appVersion="Version"
+var appVersion = "Version"
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
@@ -22,8 +22,8 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-Run: func(cmd *cobra.Command, args []string) {
-	fmt.Println(appVersion)
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(appVersion)
 	},
 }
 
